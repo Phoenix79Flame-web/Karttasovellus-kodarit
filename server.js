7a954ad81f39ea42faaac467bbe38ced25e2670e
Phoenix79Flame-web <phoenix79flameweb@gmail.com>
2020-10-08 18:10:49 +0300

    Dummy

--- server.js
+++ server.js
@@ -4,3 +4,29 @@ const app = express();
 app.listen(3000, () => console.log("kuuntelen"));
 
 app.use(express.static("public"));
+app.use(express.json({limit: 'lmb'}));
+
+const paikat = [
+  {
+    "paikka": "Helsinki",
+    "arvostelu": "kiva oli",
+    "longitud": "24.8953588",
+    "latitude": "60.19617590000001"
+  },
+  {
+    "paikka": "levi",
+    "arvostelu": "Ei niin kivaa ollut",
+    "longitude": "24.8082",
+    "latitude": "67.8840"
+  }
+]
+
+app.get('/api/paikat', function(reguest, response) {
+  response.send(paikat);
+})
+
+app.post('/api/arvostelu', function(reguest, response) {
+  console.log("Käyttäjä arvostelu");
+  console.log(reguest.body);
+  response.send(200);
+})
